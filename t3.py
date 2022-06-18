8cd7f322d3281b2fc26a1506daecc2f6d6b1ae95
Aanalpetpooja <103598785+Aanalpetpooja@users.noreply.github.com>
2022-06-18 12:25:43 +0530

    Add files via upload

---
+++ t3.py
@@ -0,0 +1,45 @@
+
+#!/usr/bin/python3
+
+# Import the required libraries
+import sys
+import os
+#import Tkinter
+#import tkMessageBox
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import subprocess
+#import git
+
+#top=Tkinter.Tk()
+# Create an instance of tkinter frame
+win = Tk()
+
+# Set the size of the tkinter window
+win.geometry("700x350")
+
+def show_msg():
+   messagebox.showinfo("Message","updating")
+
+
+def helloCallBack():
+    os.system('/home/robot4/Desktop/test/dist/test12')
+
+# Add a Label
+Label(win, text="Select a Day of the week",font=('Aerial 13')).pack(pady=10)
+
+# Create a Variable to store the selection
+var = StringVar()
+
+# Create an OptionMenu Widget and add choices to it
+option = OptionMenu(win, var, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+option.config(bg="gray81", fg="white")
+option['menu'].config(bg="green3")
+option.pack(padx=20, pady=30)
+
+ttk.Button(win, text= "update", command=helloCallBack).pack(pady= 20)#command=show_msg, 
+#b.pack()
+
+win.mainloop()
